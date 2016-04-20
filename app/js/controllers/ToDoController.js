77890bd6ebc0eb74b6bb80bd50abf35c2d8b1f6e
Patrick <p_donati@hotmail.com>
2016-04-20 12:22:07 +0100

    implemented some tests because we are rad

---
+++ app/js/controllers/ToDoController.js
@@ -0,0 +1,6 @@
+toDoApp.controller('ToDoController', [function(){
+  // var self = this;
+  this.toDos = [{text: 'ToDo1', completed: false},
+               {text: 'ToDo2', completed: true},
+               {text: 'ToDo3', completed: false}];
+  }]);
--- test/e2e/todoFeatures.js
+++ test/e2e/todoFeatures.js
@@ -4,12 +4,17 @@ describe('Todos tracker', function() {
     browser.get('/');
     expect(browser.getTitle()).toEqual('Todos App');
   });
-});
-
-describe('Todos tracker', function() {
   it('has a todo', function() {
     browser.get('/');
     var todo = $('#todo');
-    expect(todo.getText()).toEqual('ToDo1');
+    expect(todo.getText()).toContain('ToDo1');
   });
+  it('has two todos', function(){
+    browser.get('/');
+    var todo = $('#todo');
+    expect(todo.getText()).toContain('completed');
+    expect(todo.getText()).toContain('not completed');
+  });
+
+
 });
